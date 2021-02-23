3caf2b27a271373f26a9036580679262f2fd6a14
tanyatt02 <62353476+tanyatt02@users.noreply.github.com>
2021-02-23 18:02:53 +0300

    Update OAuth.js

--- config/OAuth.js
+++ config/OAuth.js
@@ -1,9 +1,9 @@
 
 let githubAuth={
     idClient:
-'6aa31e0d57b6aa9f8127',
+'***31e0d57b6aa9f8127',
     secretCode:
-    '18d824a5a4999d5530615095f82eb77762a5c8d8'}
+    '***824a5a4999d5530615095f82eb77762a5c8d8'}
 
 module.exports= githubAuth
 
